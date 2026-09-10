9d90155416bd4bddced484db913b7f9cc6871434
Nirav Raychura <raychuranirav@gmail.com>
2026-09-10 22:07:07 +0530

    Point the terradrift formula at v1.0.0.

--- Formula/terradrift.rb
+++ Formula/terradrift.rb
@@ -4,28 +4,28 @@
 class Terradrift < Formula
   desc "Plan-based Terraform and OpenTofu drift detection CLI"
   homepage "https://github.com/niravraychura/terradrift"
-  version "0.4.1"
+  version "1.0.0"
   license "MIT"
 
   on_macos do
     on_arm do
       url "https://github.com/niravraychura/terradrift/releases/download/v#{version}/terradrift_darwin_arm64.tar.gz"
-      sha256 "b1ba786a77e9452bb680f566e179e70e63242a10558c99f948ca8f19712b13af"
+      sha256 "d109ca02833087dea7e700c9953d5ed8450b8d8164ded24299f607e8bb3b8136"
     end
     on_intel do
       url "https://github.com/niravraychura/terradrift/releases/download/v#{version}/terradrift_darwin_amd64.tar.gz"
-      sha256 "b4279ec031b422f3bd144a17ab7d7472854fabb5cf477c1781202abcc90c8d41"
+      sha256 "3c868eedeec4924a5c155eb39943c64f8be893b121afc35314066dd41c3a1ee7"
     end
   end
 
   on_linux do
     on_arm do
       url "https://github.com/niravraychura/terradrift/releases/download/v#{version}/terradrift_linux_arm64.tar.gz"
-      sha256 "0e0827b804e888ae44093544ecabe42ebb7fbc07af9cc8f212785ac1b49dd1c2"
+      sha256 "f3dd31dedb28b38ab2cca902918491bdb4b55c01b8410723bd9f34e6962b4f25"
     end
     on_intel do
       url "https://github.com/niravraychura/terradrift/releases/download/v#{version}/terradrift_linux_amd64.tar.gz"
-      sha256 "5cf5057d98f6312f79d331529d335455cba57aaba350656d6cd99fa78fe8af36"
+      sha256 "7bab04014673fa10ab006d067fec0fd56a820810a1d7468eab38908e498fc9ed"
     end
   end
 
